e62da4bf56e3fb3aa9aca62e1510010d6212cb30
Emily Chen <echen@macmini1603s-Mac-mini.local>
2016-06-17 16:05:58 -0500

    added a search button but have to figure out placing and format

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  # searchable do:
+  # 	text :name, :email
+  # end
 end
